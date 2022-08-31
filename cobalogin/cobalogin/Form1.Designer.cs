34c03f65606d264747687d3958ad89fde97c97b7
Ilmanna <102424665+Ixilmanna@users.noreply.github.com>
2022-09-01 00:28:33 +0700

    Add files via upload

---
+++ cobalogin/cobalogin/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace cobalogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.textboxPass = new System.Windows.Forms.TextBox();
+            this.textboxUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LoginButton.Location = new System.Drawing.Point(313, 277);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(193, 46);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textboxPass
+            // 
+            this.textboxPass.Location = new System.Drawing.Point(313, 205);
+            this.textboxPass.Name = "textboxPass";
+            this.textboxPass.Size = new System.Drawing.Size(288, 27);
+            this.textboxPass.TabIndex = 2;
+            // 
+            // textboxUser
+            // 
+            this.textboxUser.Location = new System.Drawing.Point(313, 149);
+            this.textboxUser.Name = "textboxUser";
+            this.textboxUser.Size = new System.Drawing.Size(288, 27);
+            this.textboxUser.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "showingresult";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxUser);
+            this.Controls.Add(this.textboxPass);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox textboxPass;
+        private System.Windows.Forms.TextBox textboxUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ cobalogin/cobalogin/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cobalogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textboxUser.Text;
+            string password = textboxPass.Text;
+            string UserPass = username + password;
+
+            int UsPasLen = UserPass.Length;
+            int userlen = username.Length;
+            int passlen = username.Length;
+            int countpass =0;
+
+            Console.Write("username anda : ");
+            for (int i = 0; i < userlen; i++)
+            {
+                countpass++;
+                Console.Write(UserPass [i]);
+                
+                if (countpass == userlen){
+                Console.Write(" Password anda : ");
+                    for (int j = countpass; j < (UsPasLen); j++)
+                    {
+                        Console.Write(UserPass[j]);
+                    }
+                }
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
